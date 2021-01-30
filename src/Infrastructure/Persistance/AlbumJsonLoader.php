de74aa61895c5bcc599990de1b0962d7c495a6fa
Christopher Morton <sonro@gmx.com>
2021-01-30 12:24:55 +0000

    Add login system

--- src/Infrastructure/Persistance/AlbumJsonLoader.php
+++ src/Infrastructure/Persistance/AlbumJsonLoader.php
@@ -5,12 +5,14 @@ namespace App\Infrastructure\Persistance;
 use App\Entity\Album;
 use App\Repository\AlbumRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class AlbumJsonLoader
 {
 	public function __construct(
 		private EntityManagerInterface $entityManager,
 		private AlbumRepository $albumRepository,
+		private LoggerInterface $persistanceLogger,
 	)
 	{}
 
@@ -25,22 +27,45 @@ class AlbumJsonLoader
 	private function updateDatabase(array $data): array
 	{
 		$existingAlbums = $this->albumRepository->findAll();
+		$this->updateManagedArrayWithNewData($existingAlbums, $data);
+		$this->entityManager->flush();	
+		
+		return $existingAlbums;
+	}
+
+	private function updateManagedArrayWithNewData(array $existingAlbums, array $data)
+	{
+		$idDiff = $existingAlbums[0]->getId();
 		foreach ($data as $id => $albumData) {
-			$eId = $id - 1;
-			if (isset($existingAlbums[$eId])) {
-				$this->hydrateAlbum($existingAlbums[$eId], $albumData);
+			$exIndex = $id - $idDiff;
+			if (isset($existingAlbums[$exIndex])) {
+				if ($existingAlbums[$exIndex]->getId() !== $id) {
+					$this->logIdError($existingAlbums[$exIndex], $id);
+				} else {
+					$this->hydrateAlbum($existingAlbums[$exIndex], $albumData);
+				}
 			} else {
 				$album = new Album();
 				$this->hydrateAlbum($album, $albumData);
 				$this->entityManager->persist($album);
 			}
 		}
-		$this->entityManager->flush();	
-
-		return $existingAlbums;
 	}
 
-	private function hydrateAlbum(Album $album, array $data)
+
+	private function logIdError(Album $existing, $jsonId): void
+	{
+		$context = [
+			"dbId" => $existing->getId(),
+			"jsonId" => $jsonId
+		];
+		$this->persistanceLogger->error(
+			"Album ID mismatch between json file and database", 
+			$context
+		);
+	}
+
+	private function hydrateAlbum(Album $album, array $data): void
 	{
 		$this->changeIfUpdated($album, $data, "Artist", "artist");
 		$this->changeIfUpdated($album, $data, "Title", "title");
@@ -51,16 +76,25 @@ class AlbumJsonLoader
 		$this->changeIfUpdated($album, $data, "Year", "year");
 	}
 
-	private function changeIfUpdated(Album $album, array $data, string $Prop, string $prop)
+	private function changeIfUpdated(Album $album, array $data, string $Prop, string $prop): void
 	{
 		$getter = "get".$Prop;
 		$setter = "set".$Prop;
-		if (call_user_func([$album, $getter]) !== $data[$prop]) {
+		$existingvalue = call_user_func([$album, $getter]);
+		$newValue = $data[$prop];
+		if ($existingvalue !== $newValue) {
+			$context = [
+				"albumId" => $album->getId(),
+				"property" => $prop,
+				"from" => $existingvalue,
+				"to" => $newValue,
+			];
+			$this->persistanceLogger->debug("Album data updated", $context);
 			call_user_func([$album, $setter], $data[$prop]);
 		}
 	}
 
-	private function deserializeJson(string $json)
+	private function deserializeJson(string $json): array
 	{
 		$albums = json_decode($json, true);
 
